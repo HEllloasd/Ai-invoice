f0b77dcbf5df4cbf86fc03d82663d60eb5f0330f
Mcgabriel758 <mcgabriel0705@gmail.com>
2025-11-05 18:01:52 +0800

    Updated webhooks.ts

--- src/config/webhooks.ts
+++ src/config/webhooks.ts
@@ -1,11 +1,11 @@
 export const WEBHOOKS = {
   pdf: {
-    dropbox: 'https://real-clocks-peel.loca.lt/webhook-test/pdf-upload',
+    dropbox: 'https://good-kids-play.loca.lt/webhook-test/pdf-upload',
   },
   review: {
-    result: 'https://real-clocks-peel.loca.lt/webhook-test/review-result',
+    result: 'https://good-kids-play.loca.lt/webhook-test/review-result',
   },
   choice: {
-    receiveChoice: 'https://real-clocks-peel.loca.lt/webhook-test/receive-choice',
+    receiveChoice: 'https://good-kids-play.loca.lt/webhook-test/receive-choice',
   },
 } as const;
