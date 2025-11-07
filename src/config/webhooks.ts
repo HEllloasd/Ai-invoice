37af528c50a3c057e377909ebe96ff46f629fa4b
Mcgabriel758 <mcgabriel0705@gmail.com>
2025-11-07 22:02:18 +0800

    Updated README.md

--- src/config/webhooks.ts
+++ src/config/webhooks.ts
@@ -1,11 +1,11 @@
 export const WEBHOOKS = {
   pdf: {
-    dropbox: 'https://whole-monkeys-jam.loca.lt/webhook-test/pdf-upload',
+    dropbox: 'https://many-hotels-play.loca.lt/webhook-test/pdf-upload',
   },
   review: {
-    result: 'https://whole-monkeys-jam.loca.lt/webhook-test/review-result',
+    result: 'https://many-hotels-play.loca.lt/webhook-test/review-result',
   },
   choice: {
-    receiveChoice: 'https://whole-monkeys-jam.loca.lt/webhook-test/receive-choice',
+    receiveChoice: 'https://many-hotels-play.loca.lt/webhook-test/receive-choice',
   },
 } as const;
